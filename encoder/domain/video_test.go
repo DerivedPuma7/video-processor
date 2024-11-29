0ad1a5766925cb3ecc67534796e380969f76cd9b
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2024-11-29 16:34:17 -0300

    fix .git folder

---
+++ encoder/domain/video_test.go
@@ -0,0 +1,41 @@
+package domain_test
+
+import (
+	"testing"
+
+	"github.com/derivedpuma7/video-processor/domain"
+	"github.com/stretchr/testify/require"
+)
+
+func TestVideoIdIsNotAnUuid(t *testing.T) {
+	video, _ := domain.NewVideo("any resource id", "any path")
+	video.ID = "any id"
+
+	err := video.Validate()
+
+	require.Error(t, err)
+}
+
+func TestVideoResourceIdIsNotNull(t *testing.T) {
+	video, _ := domain.NewVideo("", "any path")
+
+	err := video.Validate()
+
+	require.Error(t, err)
+}
+
+func TestVideoFilePathIsNotNull(t *testing.T) {
+	video, _ := domain.NewVideo("any resource id", "")
+
+	err := video.Validate()
+
+	require.Error(t, err)
+}
+
+func TestVideoValidation(t *testing.T) {
+	video, _ := domain.NewVideo("any resource id", "any path")
+
+	err := video.Validate()
+
+	require.Nil(t, err)
+}
