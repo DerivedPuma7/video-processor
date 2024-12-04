5ee4e062ab603b262cdcb4054f928964a98398e1
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2024-12-04 00:02:00 -0300

    testing video download from main

--- encoder/main.go
+++ encoder/main.go
@@ -1,7 +1,28 @@
 package main
 
-import log "github.com/sirupsen/logrus"
+import (
+	"fmt"
+
+	"github.com/derivedpuma7/video-processor/application/repositories"
+	"github.com/derivedpuma7/video-processor/application/services"
+	"github.com/derivedpuma7/video-processor/domain"
+	"github.com/derivedpuma7/video-processor/framework/database"
+	log "github.com/sirupsen/logrus"
+)
+
+func prepare() (*domain.Video, repositories.VideoRepository) {
+   video, _ := domain.NewVideo("any resource id", "convite.mp4")
+   db := database.NewDbTest()
+   defer db.Close()
+   repo := repositories.VideoRepositoryDb{Db: db}
+   return video, repo
+}
 
 func main() {
 	log.Info("ola mundo")
+   video, repo := prepare()
+   videoService := services.NewVideoService(video, repo, "golang-encoder")
+
+   err := videoService.Download()
+   fmt.Println(err)
 }
